dd856f3e506ea958ff2dad8fe222fd495de0d1a9
Scott Imandt <scottimandt@gmail.com>
2024-10-30 14:25:45 -0400

    Added animation to project cards

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -1,15 +1,45 @@
 import React from 'react'
 import './CardContainer.css'
+import { motion } from 'framer-motion'
 
-function CardContainer({title, Cards}) {
+function CardContainer({ title, Cards }) {
 
     const mystyle = {
         display: "flex",
         flexWrap: "wrap",
         margin: "auto",
         justifyContent: "center",
+
+    };
+
+    const childVariants = {
+        hidden: {
+            opacity: 0,
+            y: -300,
     
-      };
+        },
+    
+        visible: {
+            y: 0,
+            opacity: 1,
+            transition: {
+                delay: .20,
+                duration: 1,
+                type: 'spring',
+                stiffness: 60,
+            }
+        },
+        exit: {
+            opacity: 0,
+            y: -1000,
+            transition: {
+                duration: .3,
+                type: 'tween',
+    
+            }
+        }
+    
+    }
 
 
     return (
@@ -21,16 +51,16 @@ function CardContainer({title, Cards}) {
                         <div className='cardProjectContainerTitle'>
                             <h1>{title}</h1>
                         </div>
-                        
-                        <div style={mystyle}>
+
+                        <motion.div style={mystyle} variants={childVariants} initial="hidden" animate="visible" exit='exit'>
 
                             {Cards}
 
-                        </div>
-                        
+                        </motion.div>
+
 
                     </div>
-                    
+
 
                 </div>
             </div>
